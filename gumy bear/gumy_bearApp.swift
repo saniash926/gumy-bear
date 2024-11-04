f93ba05755b23f681088058ba540900be28e57c5
sania shahpasand <saniash926@gmail.com>
2024-11-04 15:19:02 +0100

    Initial Commit

---
+++ gumy bear/gumy_bearApp.swift
@@ -0,0 +1,17 @@
+//
+//  gumy_bearApp.swift
+//  gumy bear
+//
+//  Created by Sania Shapasand on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct gumy_bearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
